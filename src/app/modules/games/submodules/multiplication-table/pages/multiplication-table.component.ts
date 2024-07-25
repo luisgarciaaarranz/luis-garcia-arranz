f3540baeb3357e49479dbe0b865b05ed74d2a645
LUIS <luis@MacBook-Air-de-LUIS.local>
2024-07-25 18:20:36 +0200

    complete multiplication table

--- src/app/modules/games/submodules/multiplication-table/pages/multiplication-table.component.ts
+++ src/app/modules/games/submodules/multiplication-table/pages/multiplication-table.component.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card'
 import { MatListModule } from '@angular/material/list'
 
 @Component({
   selector: 'app-multiplication-table',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatListModule],
+  imports: [CommonModule, FormsModule, MatCardModule, MatListModule],
   templateUrl: './multiplication-table.component.html',
   styleUrl: './multiplication-table.component.scss'
 })
 export class MultiplicationTableComponent {
 
   showTable: boolean = false;
+
+  multiplicatorValue: number = 0;
 
   items: number[] = [0,1,2,3,4,5,6,7,8,9]
 
@@ -21,7 +24,7 @@ export class MultiplicationTableComponent {
   }
 
   public getValue(item: number): number {
-    return 1*item
+    return this.multiplicatorValue*item
   }
 
 }
